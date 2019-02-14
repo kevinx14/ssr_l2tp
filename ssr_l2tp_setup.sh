e6bbd23a24c32c027bcf5cd27fb9a46dfd5eca53
kevinx14 <kevinx@vip.qq.com>
2019-02-14 15:06:01 +0800

    Create ssr_l2tp_setup.sh

---
+++ ssr_l2tp_setup.sh
@@ -0,0 +1,6 @@
+sudo yum update -y
+sudo yum -y install wget
+sudo wget -N –no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && sudo bash ssr.sh
+sudo chmod +x ssr.sh && sudo bash ssr.sh
+sudo bash ssr.sh
+sudo wget https://git.io/vpnsetup-centos -O vpnsetup.sh && sudo sh vpnsetup.sh

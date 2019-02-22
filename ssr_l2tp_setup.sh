8810ef363c325ee297ed747bd7530586af309854
kevinx14 <kevinx@vip.qq.com>
2019-02-22 11:51:25 +0800

    Update ssr_l2tp_setup.sh

--- ssr_l2tp_setup.sh
+++ ssr_l2tp_setup.sh
@@ -2,7 +2,6 @@ sudo yum update -y
 sudo yum -y install wget
 sudo wget -N –no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && sudo bash ssr.sh
 sudo chmod +x ssr.sh && sudo bash ssr.sh
-sudo bash ssr.sh
 sudo wget https://git.io/vpnsetup -O vpnsetup.sh && sudo \
 VPN_IPSEC_PSK='' \
 VPN_USER='' \
